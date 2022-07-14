5221bda1233ad0c74603b63663e24a8fcc24deb5
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-07-14 20:45:04 +0530

    jwt implimented
    jwt implimented

--- JWT_Authentication_Authorization/server/controller/controller.js
+++ JWT_Authentication_Authorization/server/controller/controller.js
@@ -1,5 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRECT_KEY = "Parimal";
 
 
 const signupFun = async (req, res) => {
@@ -31,10 +33,11 @@ const signupFun = async (req, res) => {
     }
 
     return res.status(201).json({ message: user });
-}
+};
 
 
 const loginFun = async (req, res) => {
+
     const { email, password } = req.body;
 
     let existUser;
@@ -50,13 +53,64 @@ const loginFun = async (req, res) => {
     }
 
     const isPasswordCorrect = bcrypt.compareSync(password, existUser.password);
-    
+
     if (!isPasswordCorrect) {
         return res.status(400).json({ message: "Invalid Password / Email" })
     }
 
-    return res.status(201).json({ message: "Succesfully Loged In" });
+    const token = jwt.sign({ id: existUser.id }, JWT_SECRECT_KEY, {
+        expiresIn: "2hr"
+    });
+
+    return res.status(201).json({ message: "Succesfully Loged In", user: existUser, token });
+};
+
+
+
+const veryfyToken = async (req, res, next) => {
+    const headers = req.headers[`authorization`];
+    // console.log(headers);
+    const token = headers.split(" ")[1];
+    if (!token) {
+        res.status(404).json({ message: "No token found" })
+    }
+
+    jwt.verify(String(token), JWT_SECRECT_KEY, (err, user) => {
+        if (err) {
+            return res.status(400).json({ message: "Invalid token" })
+        }
+        console.log(user.id);
+        req.id = user.id;
+    })
+    next();
+};
+
+
+
+
+const getUser = async (req, res, next) => {
+    const userId = req.id;
+    let user;
+    try {
+        user = await User.findById(userId, "-password")
+    } catch (error) {
+        return new Error(error);
+        // console.log(error);
+    }
+
+    if (!user) {
+        return res.status(404).json({ message: "User Not Found" })
+    }
+    return res.status(200).json({ user })
 }
+
 
 exports.signupFun = signupFun;
 exports.loginFun = loginFun;
+exports.veryfyToken = veryfyToken;
+exports.getUser = getUser;
+
+https://www.youtube.com/watch?v=gWIbT1fbLlA&t=711s
+
+11:11:05 watched
+
--- JWT_Authentication_Authorization/server/routes/route.js
+++ JWT_Authentication_Authorization/server/routes/route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signupFun, loginFun } = require("../controller/controller");
+const { signupFun, loginFun, veryfyToken, getUser } = require("../controller/controller");
 
 const router = express.Router();
 
 router.post("/signup", signupFun);
 router.post("/login", loginFun);
+router.get("/user", veryfyToken, getUser);
 
 module.exports = router;
